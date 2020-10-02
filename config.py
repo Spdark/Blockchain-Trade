03cd397a3b29c73aeb442adf834ee22109afe705
Spdark <54283080+Spdark@users.noreply.github.com>
2020-10-02 10:21:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 E_address = "techshow1999@gmail.com"
-Pass = "QWERTyuiop@123456789"
+Pass = ""
 b_msg ="We Have received your request and now its under verification so it will take 2-3 business days to verify after that you will receive an mail if you can login or not " + "\n" + "Your Regards" + "\n" + "ShipBlocks"
